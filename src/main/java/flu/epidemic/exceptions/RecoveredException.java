f66bf74b40d2cbbd291a8cb52d73bf8a24f42a9a
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-17 09:30:56 +0100

    make exceptions #3

---
+++ src/main/java/flu/epidemic/exceptions/RecoveredException.java
@@ -0,0 +1,14 @@
+package flu.epidemic.exceptions;
+
+/**
+ * @version 17/12/15.
+ */
+public class RecoveredException extends Exception {
+    public RecoveredException(){
+        super();
+    }
+
+    public RecoveredException(String message){
+        super(message);
+    }
+}
