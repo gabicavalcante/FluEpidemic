5c2784ee5c5ab7aca9848603c1ac2814cdb662a5
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-17 09:34:08 +0100

    chage the file directory #2

--- src/main/java/flu/epidemic/Enum/Being.java
+++ src/main/java/flu/epidemic/Enum/Being.java
@@ -1,13 +1,12 @@
 package flu.epidemic.Enum;
 
 /**
- * Created by Raquel on 07/12/2015.
+ * @author Raquel Oliveira
+ * @version 07/12/2015.
  */
 public enum Being {
-
     PERSON,
     CHICKEN,
     DUCK,
-    PIG;
-
+    PIG
 }
